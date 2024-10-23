09b51cf5dc8a978e512bda05371f0fbfd133abef
August <76185140+Augustina1995@users.noreply.github.com>
2024-10-23 15:46:59 +0300

    hero section - mobile version done

--- frontend/eventHiveReact/src/pages/HomePage/HomePage.tsx
+++ frontend/eventHiveReact/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,16 @@
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
+import styles from "./HomePage.module.scss";
 
 export default function HomePage() {
   return (
     <div>
         <NavigationBar />
+        <div className={styles.heroSection}>
+            <h1>Explore the hidden gems</h1>
+            <p>Discover exclusive locations and events in cities worldwide. Book your visit now!</p>
+            <button>Find out more</button>
+
+        </div>
     </div>
   )
 }
