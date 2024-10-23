cdd41260e02144a2bd30baeee4f800f416c826dc
August <76185140+Augustina1995@users.noreply.github.com>
2024-10-23 15:12:42 +0300

    navigation bar completed - static only

--- frontend/eventHiveReact/src/components/NavigationBar/NavigationBar.tsx
+++ frontend/eventHiveReact/src/components/NavigationBar/NavigationBar.tsx
@@ -1,18 +1,81 @@
+import { useState } from "react";
 import logo from "../../assets/EventHive.svg";
+import NavigationBarModal from "../Modals/NavigationBarModal/NavigationBarModal";
 import styles from "./NavigationBar.module.scss";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useMediaQuery } from "@mui/material";
 
 export default function NavigationBar() {
+  const [openModal, setOpenModal] = useState(false);
+  const isDesktop = useMediaQuery(`(min-width:900px)`);
+
+  function toggleModal() {
+    setOpenModal((prevState) => !prevState);
+  }
+
   return (
     <div className={styles.navBar}>
       <div>
-        <img src={logo} alt="Event Hive Logo" />
+        <img src={logo} alt="Event Hive Logo" className={styles.logo} />
       </div>
+
       <div className={styles.rightSide}>
-        <a href="" className={styles.navLink}>
-          Login
-        </a>
-        <MenuIcon fontSize="large" />
+        {isDesktop && (
+          <div className={styles.rightSideDesktop}>
+            <div className={styles.desktopNav}>
+              <div className={styles.buttonHover}>
+                <a href="" className={styles.navLink}>
+                  Home
+                </a>
+              </div>
+              <div className={styles.buttonHover}>
+                <a href="" className={styles.navLink}>
+                  About
+                </a>
+              </div>
+              <div className={styles.buttonHover}>
+                <a href="" className={styles.navLink}>
+                  Events
+                </a>
+              </div>
+              <div className={styles.buttonHover}>
+                <a href="" className={styles.navLink}>
+                  Contact
+                </a>
+              </div>
+            </div>
+            <div className={styles.registerLinks}>
+              <div className={styles.buttonHover}>
+                <a href="" className={styles.navLink}>
+                  Login
+                </a>
+              </div>
+              <button
+                className={`${styles.navLink} ${styles.getStartedButton}`}
+              >
+                Get Started
+              </button>
+            </div>
+          </div>
+        )}
+        {!isDesktop && (
+          <div className={styles.mobileSide}>
+            <div className={styles.buttonHover}>
+              <a href="" className={styles.navLink}>
+                Login
+              </a>
+            </div>
+            <div onClick={toggleModal}>
+              {!openModal ? (
+                <MenuIcon fontSize="large" className={styles.menuIcon} />
+              ) : (
+                <CloseIcon fontSize="large" className={styles.menuIcon} />
+              )}
+              {openModal && <NavigationBarModal />}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
