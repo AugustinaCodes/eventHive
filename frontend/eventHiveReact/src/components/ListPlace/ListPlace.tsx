87a4d1ad724447cd1fa9a648aec1a80e8ee562bd
August <76185140+Augustina1995@users.noreply.github.com>
2024-11-05 22:24:10 +0200

    .

--- frontend/eventHiveReact/src/components/ListPlace/ListPlace.tsx
+++ frontend/eventHiveReact/src/components/ListPlace/ListPlace.tsx
@@ -1,5 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../services/api";
+
+interface Event {
+    _id: string; 
+    location: string;
+    name: string;
+    description: string;
+    totalSeats: number;
+    remainingSeats: number;
+    price: number;
+    eventDate: string; 
+  }
+
 
 export default function ListPlace() {
+
+    const { data: events, isLoading, error } = useQuery<Event[]>(["events"], fetchEvents);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error loading events: {error.message}</div>;
+  }
+
   return (
     <div>
         <table>
@@ -14,7 +39,13 @@ export default function ListPlace() {
                 <th>Date of an event</th>
                 </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>
+                {events.map((event) => (
+                    <tr key={event._id}>
+                        <td>{event.name}</td>
+                    </tr>
+                ))}
+            </tbody>
 
         </table>
     </div>
